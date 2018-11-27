83726975a14c9022c6771bd0b8e26ccb732cb223
kohyejihs <ko.hjha@gmail.com>
2018-11-27 21:09:12 +0900

    ......

--- src/app/post.ts
+++ src/app/post.ts
@@ -8,3 +8,11 @@ export interface Post{
     applicant?: string[],
     file?: File,
 };
+
+// export const POSTS: Post[] = [
+//     {boardIndex: 1, boardTitle: "팀플 구합니다. 1", userId: "TEST@naver.com", boardDate: new Date().toString(), boardContent: "무슨 무슨 과목 팀플 팀원 구합니다.", memberCount: 10},
+//     {boardIndex: 2, boardTitle: "팀플 구합니다. 2", userId: "aa@naver.com", boardDate: new Date().toString(), boardContent: "팀원 구해요!", memberCount: 7},
+//     {boardIndex: 3, boardTitle: "팀플 구합니다. 3", userId: "bb@naver.com", boardDate: new Date().toString(), boardContent: "", memberCount: 10},
+//     {boardIndex: 4, boardTitle: "팀플 구합니다. 4", userId: "cc@gmail.com", boardDate: new Date().toString(), boardContent: "", memberCount: 10},
+//     {boardIndex: 5, boardTitle: "팀플 구합니다. 5", userId: "dd@gmail.com", boardDate: new Date().toString(), boardContent: "", memberCount: 10},
+// ]
