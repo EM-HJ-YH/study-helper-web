c476c90d166af6972140cecad8af0258b7f82b18
kohyejihs <ko.hjha@gmail.com>
2018-12-05 04:04:11 +0900

    마이 페이지에서 회원 탈퇴 기능 추가

--- src/app/components/my-page/my-page.component.ts
+++ src/app/components/my-page/my-page.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { User } from '../../user';
 import { AuthService } from 'src/app/auth.service';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-my-page',
@@ -13,13 +14,33 @@ export class MyPageComponent implements OnInit {
   currentUser: User;
 
   constructor(private authService: AuthService,
+              private userService: UserService,
               private router: Router) { }
 
   ngOnInit() {
+    if(this.authService.isAdmin()) {
+      this.router.navigate(['/']);
+    }
     if(this.authService.isLoggedIn()) {
       this.currentUser = this.authService.currentUser();
     } else {
       this.router.navigate(['signin']);
+    }
+  }
+
+  async deleteUser() {
+    var res = confirm("탈퇴하셔도 작성된 게시물은 삭제되지 않습니다.\nStudy Helper를 탈퇴하시겠습니까?");
+    if(res) {
+      const token: any = await this.authService.getToken();
+      this.userService
+          .deleteUser(this.currentUser.userId, token)
+          .subscribe(data => {
+            if(data.success) {
+              this.authService.logout();
+              alert('회원 탈퇴가 완료되었습니다.');
+              this.router.navigate(['signup']);
+            }
+          });
     }
   }
 }
