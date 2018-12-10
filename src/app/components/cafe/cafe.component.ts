2d1fca91c23de712daa82388e0d718da7018d78f
kohyejihs <ko.hjha@gmail.com>
2018-12-10 20:11:55 +0900

    Google Map API 추가

--- src/app/components/cafe/cafe.component.ts
+++ src/app/components/cafe/cafe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  } from 'googlemaps';
 
 @Component({
   selector: 'app-cafe',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cafe.component.css']
 })
 export class CafeComponent implements OnInit {
+  map: google.maps.Map;
+  hansung = new google.maps.LatLng(37.581753, 127.010360);
 
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: this.hansung,
+      zoom: 15,
+    };
+    this.map = new google.maps.Map(document.getElementById('map'), mapProp);
+
+    var marker = new google.maps.Marker({
+      position: this.hansung,
+      title: "한성대학교",
+      map: this.map
+    });
   }
 
 }
