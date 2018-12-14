75b801df26b355cc0038b0e2baf77b8caf1273b5
kohyejihs <ko.hjha@gmail.com>
2018-12-14 14:02:46 +0900

    예약 관리->예약 변경 기능 추가

--- src/app/components/admin/admin-cafe-book/admin-cafe-book.component.ts
+++ src/app/components/admin/admin-cafe-book/admin-cafe-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { CafeBook } from 'src/app/models/cafe';
@@ -13,10 +14,19 @@ import { CafeBookService } from 'src/app/service/cafe-book.service';
 })
 export class AdminCafeBookComponent implements OnInit {
   books: CafeBook[];
+  cafeBookEditForm: FormGroup;
+  editing: boolean = false;
+  editingCafeBook: CafeBook;
 
   constructor(private authService: AuthService,
               private cafeBookService: CafeBookService,
-              private router: Router,) { }
+              private router: Router, fb: FormBuilder) {
+    this.cafeBookEditForm = fb.group({
+      'date': [''],
+      'beginTime': [''],
+      'endTime': [''],
+    });
+  }
 
   ngOnInit() {
     if(this.authService.isAdmin()) {
@@ -46,5 +56,29 @@ export class AdminCafeBookComponent implements OnInit {
             this.ngOnInit();
           } else console.log(data.message);
         });
+  }
+
+  editClick(c: CafeBook) {
+    this.editing = !this.editing;
+    this.editingCafeBook = c;
+  }
+
+  async updateCafeBook(form: any) {
+    if(form.date!="") this.editingCafeBook.cafeBookDate = form.date;
+    if(form.beginTime!="") this.editingCafeBook.cafeBookBeginTime = form.beginTime;
+    if(form.endTime!="") this.editingCafeBook.cafeBookEndTime = form.endTime;
+    var res = confirm('수정하시겠습니까?');
+    if(res) {
+      const token: any = await this.authService.getToken();
+      this.cafeBookService
+          .updateCafeBook(this.editingCafeBook, token)
+          .subscribe(data => {
+            if(data.success) {
+              alert('예약 시간을 변경하였습니다.');
+              this.editing = false;
+              this.ngOnInit();
+            } else alert("예약 시간 변경에 실패하였습니다.\n"+data.message);
+          });
+    }
   }
 }
